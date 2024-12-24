77362b25cfc4f0bfb3fb2edfa582f7760a33d5fc
Nima Xu <472001804@qq.com>
2024-12-24 20:20:11 +0800

    Add files via upload

--- src/daemon_process.py
+++ src/daemon_process.py
@@ -1,10 +1,13 @@
 import schedule # 导入 schedule 实现定时任务执行器
 import time  # 导入time库，用于控制时间间隔
+import os
 import signal  # 导入signal库，用于信号处理
 import sys  # 导入sys库，用于执行系统相关的操作
+from datetime import datetime  # 导入 datetime 模块用于获取当前日期
 
 from config import Config  # 导入配置管理类
 from github_client import GitHubClient  # 导入GitHub客户端类，处理GitHub API请求
+from hacker_news_client import HackerNewsClient # 导入HackerNews客户端类
 from notifier import Notifier  # 导入通知器类，用于发送通知
 from report_generator import ReportGenerator  # 导入报告生成器类
 from llm import LLM  # 导入语言模型类，可能用于生成报告内容
@@ -29,6 +32,24 @@ def github_job(subscription_manager, github_client, report_generator, notifier, 
         notifier.notify(repo, report)
     LOG.info(f"[定时任务执行完毕]")
 
+def hn_topic_job(hacker_news_client, report_generator):
+    LOG.info("[开始执行定时任务]Hacker News 热点话题跟踪")
+    markdown_file_path = hacker_news_client.export_top_stories()
+    _, _ = report_generator.generate_hn_topic_report(markdown_file_path)
+    LOG.info(f"[定时任务执行完毕]")
+
+
+def hn_daily_job(hacker_news_client, report_generator, notifier):
+    LOG.info("[开始执行定时任务]Hacker News 今日前沿技术趋势")
+    # 获取当前日期，并格式化为 'YYYY-MM-DD' 格式
+    date = datetime.now().strftime('%Y-%m-%d')
+    # 生成每日汇总报告的目录路径
+    directory_path = os.path.join('hacker_news', date)
+    # 生成每日汇总报告并保存
+    report, _ = report_generator.generate_hn_daily_report(directory_path)
+    notifier.notify_hn_report(date, report)
+    LOG.info(f"[定时任务执行完毕]")
+
 
 def main():
     # 设置信号处理器
@@ -37,8 +58,8 @@ def main():
     config = Config()  # 创建配置实例
     github_client = GitHubClient(config.github_token)  # 创建GitHub客户端实例
     notifier = Notifier(config.email)  # 创建通知器实例
-    llm = LLM()  # 创建语言模型实例
-    report_generator = ReportGenerator(llm)  # 创建报告生成器实例
+    llm = LLM(config)  # 创建语言模型实例
+    report_generator = ReportGenerator(llm,config.report_types)  # 创建报告生成器实例
     subscription_manager = SubscriptionManager(config.subscriptions_file)  # 创建订阅管理器实例
 
     # 启动时立即执行（如不需要可注释）
--- src/report_generator.py
+++ src/report_generator.py
@@ -5,8 +5,23 @@ from datetime import date, timedelta
 from logger import LOG  # 导入日志模块，用于记录日志信息
 
 class ReportGenerator:
-    def __init__(self, llm):
+    def __init__(self, llm,report_types):
         self.llm = llm  # 初始化时接受一个LLM实例，用于后续生成报告
+        self.report_types = report_types
+        self.prompts = {}  # 存储所有预加载的提示信息
+        self._preload_prompts()
+
+    def _preload_prompts(self):
+        """
+        预加载所有可能的提示文件，并存储在字典中。
+        """
+        for report_type in self.report_types:  # 使用从配置中加载的报告类型
+            prompt_file = f"../prompts/{report_type}_{self.llm.model}_prompt.txt"
+            if not os.path.exists(prompt_file):
+                LOG.error(f"提示文件不存在: {prompt_file}")
+                raise FileNotFoundError(f"提示文件未找到: {prompt_file}")
+            with open(prompt_file, "r", encoding='utf-8') as file:
+                self.prompts[report_type] = file.read()
 
     def generate_daily_report(self, markdown_file_path):
         # 读取Markdown文件并使用LLM生成日报
